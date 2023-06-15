92251ab3cd775051f3dfe4524f5bc95f37937ded
MV-Data <mv.data.ai@gmail.com>
2023-06-14 21:22:52 -0300

    correcion error

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 import functions
 
 tareas = functions.get_tareas()
-checkbox_states = {}
+checkbox_states = {tarea: False for tarea in tareas}
 
 def añadir_tarea():
     tarea = st.session_state["nueva_tarea"] + "\n"
@@ -14,17 +14,18 @@ st.title("Mi Asistente Personal")
 st.subheader("Desarrollado por Alicia Linares y sus arepas.\n Soluciones de datos")
 st.write('El objetivo de esta app es incrementar su productividad')
 
+tareas_eliminar = []
+
 for tarea in tareas:
     checkbox_state = st.checkbox(tarea, key=tarea, value=checkbox_states[tarea])
     checkbox_states[tarea] = checkbox_state
-
-tareas_eliminar = [tarea for tarea, checkbox_state in checkbox_states.items() if checkbox_state]
+    if checkbox_state:
+        tareas_eliminar.append(tarea)
 
 for tarea in tareas_eliminar:
     tareas.remove(tarea)
+    del checkbox_states[tarea]
 
 functions.write_tareas(tareas)
 
 st.text_input(label="", placeholder="Ingrese Tarea", on_change=añadir_tarea, key="nueva_tarea")
-
-
